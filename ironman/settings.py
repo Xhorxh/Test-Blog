ad9ab4dab83c85f29e5aeae5d79b158b6b22d333
George <andrea.xhorxhi@gmail.com>
2022-12-20 22:08:12 +0000

    Create new app blog

--- ironman/settings.py
+++ ironman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-b!f8w4nm&q%_sz27+f*@k)4hv193zl=$fw33vi1a3s_s-#f^q9'
+#SECRET_KEY = 'django-insecure-b!f8w4nm&q%_sz27+f*@k)4hv193zl=$fw33vi1a3s_s-#f^q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
