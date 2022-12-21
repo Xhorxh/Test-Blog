7b637240833b22416e376b8fb096f037841ea268
George <andrea.xhorxhi@gmail.com>
2022-12-21 15:22:12 +0000

    Update views for Add Post section

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,9 @@ class HomeView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = ('title', 'content')
