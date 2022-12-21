b4efab4f01d8b0da870ca0241f011d73248e6d79
George <andrea.xhorxhi@gmail.com>
2022-12-21 14:26:31 +0000

    Connect URLs

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
